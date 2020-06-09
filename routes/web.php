c5a337399809ff68017b657888897729e2a32697
Lowell Stevens <lowellstevens2@gmail.com>
2020-06-09 13:46:05 +0200

    pojecte curso styde copiat entre carpetes

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    //return view('welcome');
+    return 'Home';
+});
+
+
+Route::get('/usuarios', function () {
+    //return view('welcome');
+    return 'Usuarios';
+});
+
+Route::get('/usuarios/nuevo', function () {
+   
+    return 'Crear un nuevo usuario';
+});
+
+Route::get('/usuarios/{id}', function ($id) {
+    //return view('welcome');
+    return 'Usuario con el id: ' . $id;
+})->where('id', '[0-9]+');
+
+Route::get('/saludo/{name}/{nickname?}', function ($name, $nickname = null) {
+   
+    if ($nickname)
+    {
+        return "Bienvenido {$name}, tu apodo es {$nickname}";
+    }
+    else
+    {
+        return "Bienvenido {$name}, no tienes apodo";
+    }
+        
+});
